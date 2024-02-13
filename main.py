31a1d835024d319077963bb7cf9e27b02e33ad5f
Joshua1056 <118389217+Joshua1056@users.noreply.github.com>
2024-02-13 17:34:06 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import disnake
 from disnake.ext import commands
 bot = commands.Bot()
-token = "you thought i left the token here? of course not!"
+token = "CLASSIFIED"
 
 @bot.event
 async def on_ready():
@@ -15,5 +15,17 @@ async def ping(ctx):
 @bot.slash_command(name="support", description="Link to support server")
 async def support(ctx):
     await ctx.response.send_message("discord.gg/bvmgjtjjGM")
+
+@bot.command(name='kick', description="(KICK+) Kick a user from the server")
+@commands.has_permissions(kick_members=True)  # Replace manage_messages with the desired permission
+async def kick(ctx, member: disnake.member, reason=str):
+    await member.kick(reason=reason)
+    await ctx.response.send_message(f"Kicked {member} for {reason}")
+
+@bot.command(name='ban', description="(BAN+) Ban a user from the server")
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: disnake.member, reason=str):
+    await member.ban(reason=reason)
+    await ctx.response.send_message(f"Kicked {member} for {reason}")
     
 bot.run(token)
